87806fe175e98c1faa368a44b04b3612a6de910e
Julian Scholle <Julian.Scholle@googlemail.com>
2014-10-01 21:29:32 +0200

    Stromeval

---
+++ Scripts/plot_csv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from optparse import OptionParser 
+parser = OptionParser()
+
+(optionen, args) = parser.parse_args()
+data=np.genfromtxt(args[0] , delimiter=',', dtype='float')
+plt.plot(data[:,int(args[1])], 'r-')
+
+plt.show()
+print data.shape
